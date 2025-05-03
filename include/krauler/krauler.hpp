f095ab6eda309b2278b0c66d836f97287652166e
101010zyl <zylzyl2333@gmail.com>
2025-05-03 22:36:09 +0000

    Add html_queue

--- include/krauler/krauler.hpp
+++ include/krauler/krauler.hpp
@@ -23,6 +23,22 @@ struct UrlJob {
     // 不允许复杂成员：不能用 std::string
 };
 
+struct HtmlJob {
+    char url[256];   // 固定长度字符数组，不能用 std::string
+    char html[4096]; // 固定长度字符数组，不能用 std::string
+
+    HtmlJob() = default;
+
+    HtmlJob(const char* str, const char* html_str) {
+        strncpy(url, str, sizeof(url));
+        url[sizeof(url) - 1] = '\0'; // 确保 null terminator
+        strncpy(html, html_str, sizeof(html));
+        html[sizeof(html) - 1] = '\0'; // 确保 null terminator
+    }
+
+    // 不允许复杂成员：不能用 std::string
+};
+
 class Krauler {
   public:
     // Constructor
@@ -35,10 +51,12 @@ class Krauler {
     void start();
 
   private:
-    Config                          config_;       // Configuration object
+    Config config_; // Configuration object
+
     std::unordered_set<std::string> visited_urls_; // Set of visited URLs
-    // boost::lockfree::queue<std::string*> url_queue_;       // Queue for URLs to crawl
-    boost::lockfree::queue<UrlJob> url_queue_;       // Queue for URLs to crawl
-    boost::lockfree::queue<UrlJob> extracted_queue_; // Queue for links to crawl
+
+    boost::lockfree::queue<UrlJob>  url_queue_;       // Queue for URLs to crawl
+    boost::lockfree::queue<HtmlJob> html_queue_;      // Queue for URLs to crawl
+    boost::lockfree::queue<UrlJob>  extracted_queue_; // Queue for links to crawl
 };
 } // namespace krauler
